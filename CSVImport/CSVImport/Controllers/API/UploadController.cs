7945a5dce630329ae4b0adde4570c5effdcd9d68
FrostySoup <giedrius3600@gmail.com>
2018-07-01 14:12:25 +0300

    File import in progress, files succesfully sending to backend api

---
+++ CSVImport/CSVImport/Controllers/API/UploadController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace CSVImport.Controllers.API
+{
+    [RoutePrefix("api")]
+    public class UploadController : ApiController
+    {
+        [Route("fileupload")]
+        [HttpPost]
+        [ResponseType(typeof(string))]
+        public async Task<HttpResponseMessage> Post()
+        {
+            if (Request.Content.IsMimeMultipartContent())
+            {
+                var PATH = HttpContext.Current.Server.MapPath("~/content/csvfiles");
+
+                var streamProvider = new CustomMultipartFormDataStreamProvider.CustomMultipartFormDataStreamProvider(PATH);
+                var result = await Request.Content.ReadAsMultipartAsync(streamProvider);
+
+                return Request.CreateResponse(HttpStatusCode.OK, "");
+            }
+            else
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotAcceptable, "This request is not properly formatted"));
+            }
+        }
+    }
+}
